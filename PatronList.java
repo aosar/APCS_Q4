4d9a5a86ea1dce598ce31f305ae519d7d6d7b592
snart <ariannaosar@gmail.com>
2016-05-07 10:32:11 -0700

    update patronlist

--- PatronList.java
+++ PatronList.java
@@ -6,20 +6,29 @@ public class PatronList{
   static String alphabet;
   PatronList(){
     alphabet = "abcdefghijklmnopqrstuvwxyz";
+    ArrayList<String> letterSection;
     firstChar = new ArrayList<char>();
     patron = new ArrayList<Patron>();
     patronLibrary = new ArrayList()<firstChar>; //arraylist of firstChars
+    /**
+     *From unsorted list, add patrons to each 
+     * alphabetical section
+     * 
+     */
     for(char c: alphabet.toCharArray()){
-      firstChar.add(c);
-    }
-    for(Patron p: patrons){
-      name = patron.getName();
-      nameChar = name.substring(0,1);
-      if(nameChar.equals(c))
-        patronLibrary.add(p);
-    }
+      //firstChar.add(c);
+      letterSection =  = new ArrayList<String>(); //reset the section for the next character
+      letterSection.add(c.toString());
+      for(Patron p: patrons){
+        name = patron.getName();
+        nameChar = name.substring(0,1);
+        if(nameChar.equals(c))
+          letterSection.add(p);
+      }
+      firstChar.add(letterSection);
   }
-  
+    }
+    
   public boolean addPatron(Patron newPatron){
     
   }
