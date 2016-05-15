0b2b91623f81ee5cdfcfdf683ab3e1d5843e823a
snart <ariannaosar@gmail.com>
2016-05-15 13:13:28 -0500

    updated patronlist

--- PatronList.java
+++ PatronList.java
@@ -1,15 +1,13 @@
 import java.util.ArrayList;
 
 public class PatronList{
-  public Arraylist patronLibrary;
-  public ArrayList firstChar;
+  private ArrayList<Patron> patron;
+  public ArrayList<patron> patronLibrary;
   static String alphabet;
   PatronList(){
     alphabet = "abcdefghijklmnopqrstuvwxyz";
-    ArrayList<String> letterSection;
-    firstChar = new ArrayList<char>();
-    patron = new ArrayList<Patron>();
-    patronLibrary = new ArrayList()<firstChar>; //arraylist of firstChars
+    ArrayList<Patron> patron = new ArrayList<Patron>();
+    patronLibrary = new ArrayList<Patron>(); //arraylist
     /**
      *From unsorted list, add patrons to each 
      * alphabetical section
@@ -27,10 +25,31 @@ public class PatronList{
       }
       firstChar.add(letterSection);
   }
-    }
+}
+   public Patron searchPatron(String username, String password){
+    for(Patron p: patronLibrary)
+        if(p.getUsername().compareTo(username)==0)
+            return p;
+    return null;
+  }
     
   public boolean addPatron(Patron newPatron){
-    
+    for(int i=0; i < 27; i++){
+        if(newPatron.getUsername().substring(0,1).equals(patronLibrary.get(i))) {
+            patronLibrary.get(i).add(newPatron);
+        }
+    }
   }
-
+  public boolean checkCredentials(String username, String password){
+      return searchPatron(username, password)!= null;
+  }
+  public void fillPatronIndexing(){
+        for(int i=0; i < 27; i++){
+            String currentLetter;
+            currentLetter = alphabet.substring(i,i+1);
+            patron.add(currentLetter);
+            patronLibrary.add(patron);
+        }
+    }
+  
 }
