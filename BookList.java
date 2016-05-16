83c6162efd45eb4d6cf1da20dbc84b1c30a424fc
oreojj <jj.oreo98@gmail.com>
2016-05-16 16:02:34 -0500

    Update BookList.java

--- BookList.java
+++ BookList.java
@@ -1,61 +1,16 @@
-import java.util.ArrayList;
 /**
  * Creates a list of books for the library's book archive.
  * @book Justin Orr
  *
  */
-public class BookList 
-{
-	private ArrayList<Book> bookList;
-	
+public class BookList extends List<Book>
+{	
 	/**
 	 * Constructs an book List
 	 */
 	public BookList()
 	{
-		bookList = new ArrayList<Book>();
-	}
-	
-	/**
-	 * Returns the bookList
-	 * @return bookList
-	 */
-	public ArrayList<Book> getBookList()
-	{
-		return bookList;
-	}
-	
-	/**
-	 * Sets the bookList
-	 * @param bookList
-	 */
-	public void setBookList(ArrayList<Book> bookList)
-	{
-		this.bookList = bookList;
-	}
-	
-	/**
-	 * Adds an book to the list.
-	 * @param book
-	 */
-	public void addBook(Book book) throws IsPresentException
-	{
-		if(!isPresent(book))
-			bookList.add(book); 
-		else
-			throw new IsPresentException("Book already exists.");
-	}
-	
-	/**
-	 * Adds an array of books to the list.
-	 * @param ArrayList<Book>
-	 */
-	public void add(ArrayList<Book> bookArray) throws IsPresentException
-	{
-		for(Book b: bookArray)
-		{
-			addBook(b);
-		}
+		super();
 	}
 	
 	/**
@@ -64,7 +19,7 @@ public class BookList
 	 */
 	public Book getBook(String name) throws NotPresentException
 	{
-		for(Book a: bookList)
+		for(Book a: super.getArrayList())
 		{
 			if(a.getTitle().compareTo(name)==0)
 			{
@@ -72,135 +27,6 @@ public class BookList
 			}
 		}
 		throw new NotPresentException("No Book Present");
-	}
-	
-	/**
-	 * Returns the book based on position.
-	 * @param index
-	 */
-	public Book getBook(int index) throws IndexOutOfBoundsException
-	{
-		try
-		{
-			return bookList.get(index);
-		}
-		catch(IndexOutOfBoundsException e)
-		{
-			System.out.println(e.getMessage());
-			return null;
-		}
-	}
-	
-	/**
-	 * Returns the book's index within the list.
-	 * @param book
-	 * @return index
-	 */
-	public int getBookIndex(Book book)
-	{
-		return bookList.indexOf(book);
-	}
-	
-	/**
-	 * Remove's the book from the list
-	 * @param book
-	 */
-	public void removeBook(Book book)  throws NotPresentException
-	{
-		if(isPresent(book))
-			bookList.remove(book);
-		else
-			throw new NotPresentException("Book not Present for removal.");
-	}
-	
-	/**
-	 * Remove's the book from the list.
-	 * @param index
-	 */
-	public void removeBook(int index) throws NotPresentException
-	{
-		if(bookList.get(index).equals(null) || index > size()-1)
-			throw new NotPresentException("Book not Present for removal.");
-		else
-			bookList.remove(index);			
-	}
-	
-	/**
-	 * Returns the size of the bookList
-	 * @return size
-	 */
-	public int size()
-	{
-		return bookList.size();
-	}
-	
-	/**
-	 * Compares two lists of books
-	 * @param bookList
-	 */
-	public boolean equals(BookList bookList)
-	{
-		if(this.size() != bookList.size())
-			return false;
-		else
-		{
-			for(int i = 0; i < bookList.size(); i++)
-			{
-				if(!bookList.getBook(i).equals(this.getBook(i)))
-				{
-					return false;
-				}
-			}
-			return true;
-		}
-	}
-	
-	/**
-	 * Returns whether the list is empty or not.
-	 * @return boolean
-	 */
-	public boolean isEmpty()
-	{
-		return bookList.isEmpty();
-	}
-	
-	/**
-	 * Returns the info from the book list.
-	 * @param info
-	 */
-	public String toString()
-	{
-		String info = "Book List Size: " + bookList.size();
-		return info;
-	}
-	
-	/**
-	 * Returns a boolean over whether the book already exist inside the list
-	 * @param book
-	 * @return boolean 
-	 */
-	public boolean isPresent(Book book)
-	{
-		for(Book b: bookList)
-		{
-			if(b.equals(book))
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	/**
-	 * Prints out the books from the list.
-	 */
-	public void print()
-	{
-		System.out.println("Books:");
-		for(int i = 0; i < bookList.size(); i++)
-		{
-			System.out.println(bookList.get(i).toString());
-		}
 	}
 	
 	/**
