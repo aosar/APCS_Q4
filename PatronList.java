b9679a17862c96e7f50496e076af52d195c134cc
oreojj <jj.oreo98@gmail.com>
2016-05-17 17:21:48 -0500

    Update PatronList.java

--- PatronList.java
+++ PatronList.java
@@ -1,55 +1,97 @@
 import java.util.ArrayList;
 
-public class PatronList{
-  private ArrayList<Patron> patron;
-  public ArrayList<patron> patronLibrary;
-  static String alphabet;
-  PatronList(){
-    alphabet = "abcdefghijklmnopqrstuvwxyz";
-    ArrayList<Patron> patron = new ArrayList<Patron>();
-    patronLibrary = new ArrayList<Patron>(); //arraylist
-    /**
-     *From unsorted list, add patrons to each 
-     * alphabetical section
-     * 
-     */
-    for(char c: alphabet.toCharArray()){
-      //firstChar.add(c);
-      letterSection =  = new ArrayList<String>(); //reset the section for the next character
-      letterSection.add(c.toString());
-      for(Patron p: patrons){
-        name = patron.getName();
-        nameChar = name.substring(0,1);
-        if(nameChar.equals(c))
-          letterSection.add(p);
-      }
-      firstChar.add(letterSection);
-  }
-}
-   public Patron searchPatron(String username, String password){
-    for(Patron p: patronLibrary)
-        if(p.getUsername().compareTo(username)==0)
-            return p;
-    return null;
-  }
-    
-  public boolean addPatron(Patron newPatron){
-    for(int i=0; i < 27; i++){
-        if(newPatron.getUsername().substring(0,1).equals(patronLibrary.get(i))) {
-            patronLibrary.get(i).add(newPatron);
-        }
-    }
-  }
-  public boolean checkCredentials(String username, String password){
-      return searchPatron(username, password)!= null;
-  }
-  public void fillPatronIndexing(){
-        for(int i=0; i < 27; i++){
-            String currentLetter;
-            currentLetter = alphabet.substring(i,i+1);
-            patron.add(currentLetter);
-            patronLibrary.add(patron);
-        }
-    }
-  
+/**
+ * A class that holds a list of patrons.
+ * @author Justin Orr
+ *
+ */
+public class PatronList extends List<Patron>
+{
+	/**
+	 * Default constructor
+	 */
+	public PatronList()
+	{
+		super();
+	} 
+	
+	/**
+	 * Sorts the patron list by user name.
+	 */
+	public void sortList()
+	{
+		ArrayList<Patron> patronList = super.getArrayList();
+		
+		for(int i = 0; i < patronList.size(); i++)
+		{
+			for(int j = 0; j < patronList.size(); j++)
+			{
+				if(patronList.get(j).toString().compareTo(patronList.get(i).toString()) < 0)
+				{
+					Patron p = patronList.get(j);
+					patronList.set(j, patronList.get(i));
+					patronList.set(i, p);
+				}
+			}
+		}
+		
+		super.setArrayList(patronList);
+	}
+	
+	/**
+	 * Returns the author by name
+	 * @param name
+	 */
+	public Patron getPatron(String name) throws NotPresentException
+	{
+		for(Patron p: super.getArrayList())
+		{
+			if(p.getName().compareTo(name)==0)
+			{
+				return p;
+			}
+		}
+		throw new NotPresentException("No Patron Present");
+	}
+	
+	/**
+	 * Searches the patron list for a patron with the specified credentials.
+	 * @param username
+	 * @param password
+	 * @return patron
+	 * @throws NotPresentException
+	 */
+	public Patron searchPatronList(String username, String password) throws NotPresentException
+	{
+		for(Patron p: super.getArrayList())
+		{
+			if(p.getUsername().compareTo(username)==0 && p.getPasswd().compareTo(password)==0)
+				return p;
+		}
+		throw new NotPresentException("There is no one with those credentials availible.");
+	}
+	
+	/**
+	 * Checks to see if there is a patron with the specified credentials.
+	 * @param username
+	 * @param password
+	 * @return boolean
+	 */
+	public boolean checkCredentials(String username, String password)
+	{
+		for(Patron p: super.getArrayList())
+		{
+			if(p.getUsername().compareTo(username)==0 && p.getPasswd().compareTo(password)==0)
+				return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Displays the person information in a GUI.
+	 */
+	public void display()
+	{
+		//Execute GUI code.
+	}
 }
